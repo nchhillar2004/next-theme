f75b98ee2b0d62fdea5797a69290ff2021da65a8
Nishant Chhillar <nchhillar2004@gmail.com>
2024-09-15 10:02:44 +0530

    footer

--- components/site-footer.tsx
+++ components/site-footer.tsx
@@ -1,6 +1,6 @@
 export default function SiteFooter() {
     return (
-        <footer>
+        <footer className="py-2">
             <div className="container text-center">
                 <p className="text-xs text-gray-600 dark:text-gray-400">&copy; 2024 - 2025, @nchhillar2004 - GitHub</p>
             </div>
